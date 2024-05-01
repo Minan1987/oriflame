59630239b063bd0c9eb329a48f185aea783aa83a
Mina Nazari <mina.nazary66@gmail.com>
2024-05-01 11:11:30 +0330

    images address modified in Data.jsx and project build successfully

--- shop/src/components/productCategory/Data.jsx
+++ shop/src/components/productCategory/Data.jsx
@@ -1,99 +1,119 @@
+import imageCat01 from '../../assets/img/small-item-18.jpg'
+import imageCat02 from '../../assets/img/small-item-19.jpg'
+import imageCat03 from '../../assets/img/small-item-16.jpg'
+import imageCat04 from '../../assets/img/small-item-17.jpg'
+import imageCat05 from '../../assets/img/small-item-20.jpg'
+import imageCat06 from '../../assets/img/small-item-21.jpg'
+import imageCat07 from '../../assets/img/small-item-23.jpg'
+import imageCat08 from '../../assets/img/small-item-24.jpg'
+import imageCat09 from '../../assets/img/small-item-29.jpg'
+import imageCat10 from '../../assets/img/small-item-30.jpg'
+import imageCat11 from '../../assets/img/small-item-31.jpg'
+import imageCat12 from '../../assets/img/small-item-33.jpg'
+import imageCat13 from '../../assets/img/small-item-25.jpg'
+import imageCat14 from '../../assets/img/small-item-26.jpg'
+import imageCat15 from '../../assets/img/small-item-27.jpg'
+import imageCat16 from '../../assets/img/small-item-28.jpg'
+import imageCat17 from '../../assets/img/category-1.jpg'
+import imageCat18 from '../../assets/img/category-2.jpg'
+import imageCat19 from '../../assets/img/category-4.jpg'
+import imageCat20 from '../../assets/img/category-6.jpg'
 const menu = [
     {
         id: 1,
         title: "ماسک پوست",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-20.WEBP"
+        img: imageCat05
     },
     {
         id: 2,
         title: "کرم ضد آفتاب",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-21.WEBP"
+        img: imageCat06
     },
     {
         id: 3,
         title: "مراقبتی لب و چشم",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-23.jpg"
+        img: imageCat07
     },
     {
         id: 4,
         title: "ابزار مراقبت از پوست",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-24.jpg"
+        img: imageCat08
     },
     {
         id: 5,
         title: "سِرم و درمان",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-18.WEBP"
+        img: imageCat01
     },
     {
         id: 6,
         title: "کمپین خرید محصولات Novage+",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-19.jpg"
+        img: imageCat02
     },
     {
         id: 7,
         title: "پاک کننده و تونر",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-16.jpg"
+        img: imageCat03
     },
     {
         id: 8,
         title: "مرطوب کننده",
         category: "محصولات مراقبتی پوست",
-        img: "/src/assets/img/small-item-17.WEBP"
+        img: imageCat04
     },
     {
         id: 9,
         title: "شامپو",
         category: "محصولات مراقبتی مو",
-        img: "/src/assets/img/small-item-29.jpg"
+        img: imageCat09
     },
     {
         id: 10,
         title: "نرم کننده و تقویت کننده مو",
         category: "محصولات مراقبتی مو",
-        img: "/src/assets/img/small-item-30.jpg"
+        img: imageCat10
     },
     {
         id: 11,
         title: "ژل، موس و اسپری مو",
         category: "محصولات مراقبتی مو",
-        img: "/src/assets/img/small-item-31.jpg"
+        img: imageCat11
     },
     {
         id: 12,
         title: "ماسک مو",
         category: "محصولات مراقبتی مو",
-        img: "/src/assets/img/small-item-33.jpg"
+        img: imageCat12
     },
     {
         id: 13,
         title: "عطر زنانه",
         category: "عطر و ادکلن",
-        img: "/src/assets/img/small-item-25.jpg"
+        img: imageCat13
     },
     {
         id: 14,
         title: "عطر مردانه",
         category: "عطر و ادکلن",
-        img: "/src/assets/img/small-item-26.jpg"
+        img: imageCat14
     },
     {
         id: 15,
         title: "مام و اسپری ضد تعریق",
         category: "عطر و ادکلن",
-        img: "/src/assets/img/small-item-27.jpg"
+        img: imageCat15
     },
     {
         id: 16,
         title: "بادی اسپلش",
         category: "عطر و ادکلن",
-        img: "/src/assets/img/small-item-28.jpg"
+        img: imageCat16
     },
 ]
 export default menu
